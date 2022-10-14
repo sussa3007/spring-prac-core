8ff537861085e4db6c3f497b0e476691260e8982
sussa3007 <sussa1933@gmail.com>
2022-10-14 15:41:29 +0900

    Add #1 - DiscountPolicy Update
    할인 정책 추가 및
    할인 정책 기능의 확장으로 OrderServiceImpl의 코드 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,28 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicyImpl;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository
             = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy
-            = new FixDiscountPolicyImpl();
+            = new RateDiscountPolicy();
+    /**
+     * DIP 위반
+     * OrderServiceImpl 는 DiscountPolicy 인터페이스 뿐만아니라
+     * RateDiscountPolicy 도 의존 하고 있다.
+     * OCP 위반
+     * RateDiscountPolicy 구현함으로서 기능을 확장하였다.
+     * 기능 확장함과 동시에 "OrderServiceImpl 클래스의 코드를 변경해야한다."
+     *     private final DiscountPolicy discountPolicy
+     *             = new FixDiscountPolicyImpl();
+     */
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
